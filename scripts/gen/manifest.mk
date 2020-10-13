b619c63c033426a8523b2a81628ac7347172b13d
Jeffrey Stoke <me@arhat.dev>
2020-10-13 22:29:38 +0200

    Update helm chart

--- scripts/gen/manifest.mk
+++ scripts/gen/manifest.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+NS ?= default
+
 gen.manifests.renovate-server:
 	helm template \
 		renovate-server \
