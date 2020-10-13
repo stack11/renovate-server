bbe58b6d7559f350eb472fc4341fd2cf1e8c1f8b
Jeffrey Stoke <me@arhat.dev>
2020-10-13 22:31:40 +0200

    Fix build tags

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -32,7 +32,7 @@ _build() {
 
 renovate_server() {
   # TODO: set mandatory tags and predefined tags for specific platforms
-  _build "CGO_ENABLED=0 ${GOBUILD} -tags='nokube nocloud netgo ${PREDEFINED_BUILD_TAGS} ${TAGS}' ./cmd/renovate-server"
+  _build "CGO_ENABLED=0 ${GOBUILD} -tags='netgo ${PREDEFINED_BUILD_TAGS} ${TAGS}' ./cmd/renovate-server"
 }
 
 COMP=$(printf "%s" "$@" | cut -d. -f1)
