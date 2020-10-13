f3e19fefb9c1fa2f782667d5094f0c9a6ce5cd1c
Jeffrey Stoke <me@arhat.dev>
2020-10-13 22:29:12 +0200

    Remove basic auth

--- pkg/conf/auth.go
+++ pkg/conf/auth.go
@@ -1,17 +1,9 @@
 package conf
 
-type BasicAuthConfig struct {
-	Username string `json:"username" yaml:"password"`
-	Password string `json:"password" yaml:"password"`
-
-	OTP string `json:"otp" yaml:"otp"`
-}
-
 type OAuthConfig struct {
 	Token string `json:"token" yaml:"token"`
 }
 
 type APIAuthConfig struct {
-	OAuth *OAuthConfig     `json:"oauth" yaml:"oauth"`
-	Basic *BasicAuthConfig `json:"basic" yaml:"basic"`
+	OAuth *OAuthConfig `json:"oauth" yaml:"oauth"`
 }
